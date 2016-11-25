7783e11322770c54c4136f408123687197f40e87
galaterro <adrix1992@gmail.com>
2016-11-25 23:18:40 +0100

    some useful changes.

---
+++ src/FileReader.java
@@ -0,0 +1,45 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+
+public class FileReader {
+
+    public static void readFile(){
+        try{
+            File file = new File("fichPersona.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+
+
+            NodeList nl = doc.getElementsByTagName("personas");
+
+            for (int i = 0; i < nl.getLength(); i++) {
+                Node n = nl.item(i);
+                Element elem = (Element) n;
+
+                for (int j = 0; j < ((Element) n).getElementsByTagName("persona").getLength(); j++) {
+                    System.out.println(elem.getAttribute("id") +"\t\t" + elem.getElementsByTagName("nombre").item(j).getTextContent() + "\t\t" +
+                            elem.getElementsByTagName("apellido").item(j).getTextContent() + "\t\t" +
+                            elem.getElementsByTagName("profesion").item(j).getTextContent());
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -28,6 +37,7 @@ public class Main {
 
             alp1.add(p2);
             FileGenerator.xmlGenerator(alp1);
+            FileReader.readFile();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
